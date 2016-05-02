2e42c1c23e0d4b19160143f218043f88dce5253d
Vivek Nair <vivekaxl@gmail.com>
2016-05-02 00:20:49 -0400

    driver program done

---
+++ Problems/CPM/driver.sh
@@ -0,0 +1,11 @@
+python new_cpm_reduction_research_question1_2_apache.py &
+python new_cpm_reduction_research_question1_2_bdbc.py &
+python new_cpm_reduction_research_question1_2_bdbj.py &
+python new_cpm_reduction_research_question1_2_llvm.py &
+python new_cpm_reduction_research_question1_2_sql.py &
+python new_cpm_reduction_research_question1_2_x264.py &
+
+
+wait
+
+echo "Finished"
